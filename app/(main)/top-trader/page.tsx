27d43b3f961c9c98acd73f9883b4b6b07a11e26d
leminphuong <phuonghole121201@gmail.com>
2026-01-06 16:26:24 +0700

    done

---
+++ app/(main)/top-trader/page.tsx
@@ -0,0 +1,16 @@
+export default async function Home() {
+  return (
+    <div className="w-screen h-screen">
+      <iframe
+        src="https://coin.coinjdg.com/top-trader/"
+        title="Club de Leonardo AI Brasil"
+        style={{
+          width: "100%",
+          height: "100%",
+          border: "none",
+        }}
+        allowFullScreen
+      />
+    </div>
+  );
+}
